b5feb6189fb2c72d9472af5642dc9f1729b3b25d
Thiago Santos <thiagoss1995@gmail.com>
2021-08-19 00:37:03 -0300

    Resolução do desafio de conceitos NodeJS

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +11,68 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const likes = 0;
+  const repository = { id: uuid(), title, url, techs, likes };
+  
+  //Adiciona o novo repositório na lista de repositórios
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repository = {id, title, url, techs};
+
+  //Verifica o índice do repositório com id informado
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: "Repository not found."});
+  }
+
+  //Mescla as propriedades do repositório atual com as alterações do update
+  const currentRepository = repositories[repositoryIndex];
+  const newRepository = {...currentRepository, ...repository};
+
+  //Atualiza o repositório informado
+  repositories[repositoryIndex] = newRepository;
+
+  return response.json(newRepository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  //Verifica o índice do repositório com id informado
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: "Repository not found."});
+  }
+
+  //Faz a remoção do repositório informado
+  repositories.splice(repositoryIndex, 1);
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  //Verifica o índice do repositório com id informado
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  if(repositoryIndex < 0){
+    return response.status(400).json({error: "Repository not found."});
+  }
+
+  //Adiciona um like ao repositório informado
+  repositories[repositoryIndex].likes++;
+
+  const repository = repositories[repositoryIndex];
+  return response.json(repository);
 });
 
 module.exports = app;
